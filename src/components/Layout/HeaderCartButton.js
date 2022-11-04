f203a8b2027f2f314f4b79e73fa160b2a08abec9
Carlos Javier <cjangie13@gmail.com>
2022-11-04 05:57:25 -0400

    app done

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-content';
 
 const HeaderCartButton = (props) => {
+    const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false)
     const cartCtx = useContext(CartContext)
-    const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+    const { items } = cartCtx
+    const numberOfCartItems = items.reduce((currentNum, item) => {
         return currentNum + item.amount;
     }, 0);
 
+
+    const btnClasses = `${classes.button} ${buttonIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) return;
+        setButtonIsHighlighted(true)
+
+        const timer = setTimeout(() => {
+            setButtonIsHighlighted(false)
+        }, 300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
