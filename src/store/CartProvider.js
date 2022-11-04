fe5fb3679f97bde248702ae6c1fb9d290108b805
Carlos Javier <cjangie13@gmail.com>
2022-11-03 20:57:58 -0400

    adding context adn reducer to handle adding and removing items to the cart

---
+++ src/store/CartProvider.js
@@ -0,0 +1,44 @@
+import React, { useReducer } from 'react';
+import CartContext from './cart-content';
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, actions) => {
+    if (actions.type === 'ADD_ITEM') {
+        const updatedItems = state.items.concat(actions.payload);
+        const updatedTotalAmount = state.totalAmount + actions.payload.price * actions.payload.amount;
+        return { items: updatedItems, totalAmount: updatedTotalAmount }
+    }
+    return defaultCartState
+}
+
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({ type: 'ADD_ITEM', payload: item })
+    }
+
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({ type: 'REMOVE_ITEM', id: id })
+    }
+
+    const cartContext = {
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    }
+
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    );
+}
+
+export default CartProvider;
